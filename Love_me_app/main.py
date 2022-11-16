f80bdc420dcfbb449d0c21ecacc7b510733d1186
Eti-Umoh <etiumoh04@gmail.com>
2022-11-16 22:04:11 +0100

    app structure set up

---
+++ Love_me_app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from .import models
+from .database import engine
+from .routers import ai_trainer,authentication,letter,schedule,subscription,transaction,users
+
+app = FastAPI(
+    title="LoveMeApp",
+    description="You don't know how to share your deepest feelings? Why not let an AI write love letters for you? Schedule it so it generates love letters and sends to your loved ones weekly for a small fee.",
+)
+models.Base.metadata.create_all(engine)
+
+# app.include_router(ai_trainer.router)
+# app.include_router(authentication.router)
+# app.include_router(letter.router)
+# app.include_router(schedule.router)
+# app.include_router(subscription.router)
+# app.include_router(transaction.router)
+# app.include_router(users.router)
---
+++ Love_me_app/schemas.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel
+from datetime import date,time,datetime
+
+class User(BaseModel):
+    first_name:str
+    last_name:str
+    password:str
+    email:str
+    facebook_id:str
+    google_id:str
+    is_sub_active:bool
+    sub_end_date:datetime
+    is_reminder:bool
+    date_created:datetime
+
+class Receiver(BaseModel):
+    name:str
+    email:str
+    phone_number:str
+    user_id:int
+    date_created:datetime
+
+class Letter(BaseModel):
+    user_id:int
+    receiver_id:int
+    letter:str
+    date_created:datetime
+
+class Schedule(BaseModel):
+    user_id:int
+    receiver_id:int
+    schedule_time:datetime
+    date_created:datetime
+
+class AiTrainer(BaseModel):
+    ui_name:str
+    ai_word:str
+    date_created:datetime
+
+class AiTrainerValue(BaseModel):
+    ai_trainer_id:int
+    user_id:int
+    receiver_id:int
+    value:str
+    date_created:datetime
+
+class Subscription(BaseModel):
+    name:str
+    description:str
+    months:int
+    total_sms:int
+    amount:int
+    date_created:datetime
+
+class Transaction(BaseModel):
+    user_id:int
+    subscription_id:int
+    ref_no:str
+    date_created:str
+
+class ResetPass(BaseModel):
+    pin:str
+    user_id:int
+    is_used:bool
+    expiry_date:datetime
+    date_created:datetime
+
+class BlackListedTokens(BaseModel):
+    token:str
+    expiry_date:datetime
+    blacklisted_on:datetime
