bf123241808032277c9aec031398c79dc3729fae
Afeez G Lawal <afeezlg@gmail.com>
2022-11-17 15:40:32 +0100

    Added generate letter functionality.

--- Love_me_app/utils.py
+++ Love_me_app/utils.py
@@ -1,2 +1,17 @@
-def send_sms(recipient, text):
-    pass
+import os
+
+import openai
+from twilio.rest import Client
+
+twilio_account_sid = os.environ.get("TWILIO_ACCOUNT_SID", None)
+twilio_auth_token = os.environ.get("TWILIO_AUTH_TOKEN", None)
+twilio_phone_number = os.environ.get("TWILIO_PHONE_NUMBER", None)
+twilio_client = Client(twilio_account_sid, twilio_auth_token)
+openai.api_key = os.environ.get("OPENAI_API_KEY", None)
+
+
+def generate_letter(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=6
+    )
+    return response
