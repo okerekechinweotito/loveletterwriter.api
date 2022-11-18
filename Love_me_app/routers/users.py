928a4225840ca713a013df87baa9708384a8e4ee
aaronben41 Github <aaronben41@gmail.com>
2022-11-18 14:32:02 +0100

    feat(BAC-2): modified user endpoints

--- Love_me_app/routers/users.py
+++ Love_me_app/routers/users.py
@@ -1,23 +1,28 @@
+"""
+User endpoints for resetting password
+Modules needed for these endpoints that are being worked on by others
+have been 'simulated' in the dependencies.py file and have been imported
+This is to ensure that once the branches are merged, everything should 
+work as expected
+"""
+
 #Import libraries
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
+
+from dependencies import get_current_user, generate_access_token, \
+    get_password_hash, password_reset, website_url
 
 #Import modules
 import schemas, models
 from database import get_db
-from utils import get_password_hash, generate_access_token
-from send_email import password_reset
 
 #Initialize router and db
 db = get_db()
 router = APIRouter()
 
-#create dummy variables and functions for endpoints
-#pending when teammates will complete their tasks
-website_url = "www.mechanic-team-engine.com"
-
 
 #Endpoints
-# TODO: Reset password routes
+# TODO: Reset password request route
 
 @router.post("/user/reset_request/", response_description="Password reset request")
 async def reset_request(user_email: schemas.User):
@@ -43,11 +48,31 @@ async def reset_request(user_email: schemas.User):
             detail="Your details not found, invalid email address"
         )
 
-
+# TODO: Reset password route
 @router.patch("user/reset_password/", response_description="Password reset")
-async def reset_password(password_reset: schemas.ResetPass):
+async def reset_password(token: str, password_reset: schemas.User):
     reset_pass_data = {k:v for k, v in password_reset.dict().items() if v is not None}
 
-    reset_pass_data["new_password"] = get_password_hash()
-    return ("Successful")
+
+    reset_pass_data["password"] = get_password_hash(reset_pass_data["new_password"])
+    
+    if len(reset_pass_data) >= 1:
+        # Get current user from session token
+        user = await get_current_user(token)
+
+        # Modify password of the current user
+        update_result = await db["users"].update_one({"id": user["id"]}, {"$set": reset_pass_data})
+
+        if update_result.modified_count == 1:
+            # get the newly updated current user and return as a response
+            updated_user = await db["users"].find_one({"id": user["id"]})
+            if(updated_user) is not None:
+                return updated_user
+
+    existing_user = await db["users"].find_one({"id": user["id"]})
+    if(existing_user) is not None:
+        return existing_user
+
+    # Raise error if a user with that email and id can not be found in the database
+    raise HTTPException(status_code=404, detail=f"User not found")
 
