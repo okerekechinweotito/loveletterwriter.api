5ebcb2e50db7f31d790413623ef5e6caf35b798e
YetundeAlabi <alabiyetunde007@gmail.com>
2022-11-18 21:03:05 +0100

    feat(BAC-7): added endpoint to patch user profile

--- Love_me_app/routers/users.py
+++ Love_me_app/routers/users.py
@@ -1,26 +1,27 @@
 from dependencies import get_current_user
 from fastapi import Depends, APIRouter, HTTPException
 from .. import schemas
+from ..database import get_db
+from sqlalchemy.orm import Session 
 
-router=APIRouter()
+router=APIRouter(tags=['User'],prefix="/api/v1/user/me")
 
 
-@router.get("/user/me", dependencies=[Depends(get_current_user())])
+@router.get("/", dependencies=[Depends(get_current_user())])
 def user_me(user: schemas.User):
-    if not user:
-        raise HTTPException(status_code=404, detail="user not found")
+    if user is None:
+        raise HTTPException(status_code=401, detail="user not found")
     return {
-        "firstname": user.firstname,
-        "lastname": user.lastname,
+        "firstname": user.first_name,
+        "lastname": user.last_name,
         "email": user.email,
         "is_active": user.is_sub_active,
         "is_reminder": user.is_reminder,
-        "date_joined": user.date_joined,
+        "date_joined": user.date_created,
     }
 
-@router.patch("/user/me", dependencies=[Depends(get_current_user())])
-def update_profile(user:dict= Depends(get_current_user), db:Session = Depends(get_db)):
-
+@router.patch("/", dependencies=[Depends(get_current_user())])
+def update_profile(user: schemas.User, db:Session = Depends(get_db)):
     user.update(user.dict(exclude_unset=True))
     db.commit()
     return {"User successfully updated"}
