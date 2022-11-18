cd8209d8126eb6e0dd6dff5838fd4bf8bebadcfc
Afeez G Lawal <afeezlg@gmail.com>
2022-11-18 08:10:54 +0100

    Added email sending functionality.

--- Love_me_app/utils.py
+++ Love_me_app/utils.py
@@ -1,13 +1,23 @@
 import os
 
+import boto3
 import openai
+from botocore.exceptions import ClientError
 from twilio.rest import Client
 
+# Setup Twilio
 twilio_account_sid = os.environ.get("TWILIO_ACCOUNT_SID", None)
 twilio_auth_token = os.environ.get("TWILIO_AUTH_TOKEN", None)
 twilio_phone_number = os.environ.get("TWILIO_PHONE_NUMBER", None)
 twilio_client = Client(twilio_account_sid, twilio_auth_token)
+
+# Setup openai
 openai.api_key = os.environ.get("OPENAI_API_KEY", None)
+
+# Setup SES
+FROM_EMAIL = os.environ.get("FROM_EMAIL", None)
+AWS_REGION = os.environ.get("AWS_REGION", None)
+email_client = boto3.client("ses", region_name=AWS_REGION)
 
 
 def generate_letter(prompt):
@@ -24,7 +34,82 @@ def generate_letter(prompt):
     return text
 
 
-def send_sms(reciever, text):
+def send_sms(letter):
     twilio_client.messages.create(
-        body=text, from_=twilio_phone_number, to=reciever.phone_number
+        body=letter.letter, from_=twilio_phone_number, to=letter.reciever.phone_number
     )
+
+
+def send_email(letter):
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = letter.letter
+
+    # The HTML body of the email.
+    BODY_HTML = f"""<html>
+        <head></head>
+        <body>
+            <h1>Love letter from {letter.sender.name}</h1>
+            {letter.letter}
+        </body>
+        </html>
+    """
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = email_client.send_email(
+            Destination={
+                "ToAddresses": [
+                    letter.reciever.email,
+                ],
+            },
+            Message={
+                "Body": {
+                    "Html": {
+                        "Charset": "UTF-8",
+                        "Data": BODY_HTML,
+                    },
+                    "Text": {
+                        "Charset": "UTF-8",
+                        "Data": BODY_TEXT,
+                    },
+                },
+                "Subject": {
+                    "Charset": "UTF-8",
+                    "Data": f"Love letter from {letter.sender.name}",
+                },
+            },
+            Source=FROM_EMAIL,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        print("Email sent! Message ID:"),
+        print(response["MessageId"])
+
+
+# mock_prompt = "Write a letter to Shade telling her I love her so much in a romantic fashion, from Afeez"
+
+# message = generate_letter(mock_prompt)
+
+
+# class MockSender:
+#     name: str = "Afeez"
+#     email: str = "lafrizfz@gmail.com"
+
+
+# class MockReciever:
+#     name: str = "Afeez"
+#     email: str = "afeezlg@gmail.com"
+#     phone_number: str = "+2348162302855"
+
+
+# class Letter:
+#     letter: str = message
+#     reciever = MockReciever
+#     sender = MockSender
+
+
+# send_sms(Letter)
+# send_email(Letter)
