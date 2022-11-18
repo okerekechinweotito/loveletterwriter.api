113b878ba65ebc49bfc7c4e7a05a60fe2a62848d
Eti-Umoh <etiumoh04@gmail.com>
2022-11-18 23:24:10 +0100

    added authentication to receiver endpoints

--- Love_me_app/routers/receiver.py
+++ Love_me_app/routers/receiver.py
@@ -23,19 +23,28 @@ def create_receiver(payload: schemas.Receiver, user:dict=Depends(get_current_use
     return new_receiver 
 
 @router.get('/{receiver_id}',response_model=schemas.DisplayReceiver)
-def get_receiver(id, response: Response, db:Session = Depends(get_db)):
+def get_receiver(id, response: Response,user:dict=Depends(get_current_user), db:Session = Depends(get_db)):
+    user = user
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Please log in")
     receiver = db.query(models.Receiver).filter(models.Receiver.id == id).first()
     if not receiver:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Receiver not found")
     return receiver
 
 @router.get('/',response_model=List[schemas.DisplayReceiver])
-def get_receivers(response: Response, db:Session = Depends(get_db)):
+def get_receivers(response: Response,user:dict=Depends(get_current_user), db:Session = Depends(get_db)):
+    user = user
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Please log in")
     receivers = db.query(models.Receiver).all()
     return receivers
 
 @router.patch('/{receiver_id}')
-def patch_receiver(id, request:schemas.Receiver, db:Session = Depends(get_db)):
+def patch_receiver(id, request:schemas.Receiver,user:dict=Depends(get_current_user), db:Session = Depends(get_db)):
+    user = user
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Please log in")
     receiver = db.query(models.Receiver).filter(models.Receiver.id == id)
     if not receiver.first():
         pass
@@ -44,7 +53,10 @@ def patch_receiver(id, request:schemas.Receiver, db:Session = Depends(get_db)):
     return {'Receiver successfully updated'}
 
 @router.delete('/{receiver_id}')
-def delete_receiver(id, db:Session = Depends(get_db)):
+def delete_receiver(id,user:dict=Depends(get_current_user), db:Session = Depends(get_db)):
+    user = user
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Please log in")
     db.query(models.Receiver).filter(models.Receiver.id == id).delete(synchronize_session=False)
     db.commit()
     return {"Receiver successfully deleted"}
