36ba8cf9d7931016fda1a2dfa4eb2b719a877227
YetundeAlabi <alabiyetunde007@gmail.com>
2022-11-18 20:48:17 +0100

    feat(BAC-7): modified get user endpoint'

--- Love_me_app/routers/users.py
+++ Love_me_app/routers/users.py
@@ -1,10 +1,14 @@
 from dependencies import get_current_user
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from .. import schemas
+
 router=APIRouter()
-@router.get("/user/me")
-def user_me(user:dict=Depends(get_currrent_user)):
-    #   return {'user':user}
+
+
+@router.get("/user/me", dependencies=[Depends(get_current_user())])
+def user_me(user: schemas.User):
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
     return {
         "firstname": user.firstname,
         "lastname": user.lastname,
@@ -14,7 +18,7 @@ def user_me(user:dict=Depends(get_currrent_user)):
         "date_joined": user.date_joined,
     }
 
-@router.patch("/user/me")
+@router.patch("/user/me", dependencies=[Depends(get_current_user())])
 def update_profile(user:dict= Depends(get_current_user), db:Session = Depends(get_db)):
 
     user.update(user.dict(exclude_unset=True))
