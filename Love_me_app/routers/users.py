01f892807c183f3e35a55e5a3acacc18f873202c
YetundeAlabi <alabiyetunde007@gmail.com>
2022-11-18 19:47:30 +0100

    added user get request

---
+++ Love_me_app/routers/users.py
@@ -0,0 +1,25 @@
+from dependencies import get_current_user
+from fastapi import Depends, APIRouter
+from fastapi import APIRouter
+router=APIRouter()
+@router.get('/user/me')
+def user_me(user:dict=Depends(get_currrent_user)):
+    #   return {'user':user}
+    return {
+        "firstname": user.firstname,
+        "lastname": user.lastname,
+        "email": user.email,
+        "is_active": user.is_sub_active,
+        "is_reminder": user.is_reminder,
+        "date_joined": user.date_joined,
+    }
+
+@router.patch("api/v1/user/me")
+def update_profile(user:dict= Depends(get_current_user)):
+
+    user.update(user.dict(exclude_unset=True))
+    return {
+        "firstname": ,
+        "lastname": ,
+        "is_active": ,
+    }
