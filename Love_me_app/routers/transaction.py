63a14591955194373e49510871e9d51cf7b76a62
madvirus-ops <edwinayabie1@gmail.com>
2022-11-19 12:24:14 +0100

    chore(BAC-5) removed unused codes

--- Love_me_app/routers/transaction.py
+++ Love_me_app/routers/transaction.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,Request
+from fastapi import APIRouter,Depends,Request,Header
 from datetime import timedelta
 from fastapi.responses import Response
 import stripe
@@ -10,9 +10,9 @@ router = APIRouter(tags=['transactions'])
 
 
 @router.post("/completed",description="transaction webhook")
-async def completed(requests:Request):
-    payload = requests.body
-    sig_header = requests.headers.get('STRIPE_SIGNATURE')
+async def completed(requests:Request,stripe_signature:str = Header(str)):
+    payload =await requests.body()
+    sig_header = stripe_signature
     endpoint_secret = 'whsec_9408e100f9b71cae7e32ce9f54927bb1f2f76a88dfe62d9793e5d0ce16617066'
     event = None
     try:
@@ -23,13 +23,26 @@ async def completed(requests:Request):
         raise e
     except stripe.error.SignatureVerificationError as e:
         raise e
-    if event.type == 'payment_intent.succeeded':
-        print(event['metadata']['email'])
+    if event.type == 'checkout.session.completed':
         # user.is_sub_active = True
         # user.sub_end_date = timedelta(days=30)
-        # payment_intent = event.data.object # contains a stripe.PaymentIntent
-        print('PaymentIntent was successful!')
-    print('Handled event type {}'.format(event['type']))
+        payment_intent = event.data # contains a stripe.PaymentIntent
+        print('PaymentIntent was successful!',payment_intent)
+        details = {
+            'user_name':event.data.metadata.user_name,
+            'user_email':event.data.metadata.user_email,
+            'user_id':event.data.metadata.user_id,
+            'plan_type':event.data.metadata.plan_type,
+            'month':event.data.metadata.month,
+            'total_sms':event.data.metadata.totel_sms
+        }
+        print(details)
 
-    return Response(success=True)
+    print('Handled event type {}'.format(event['type']))
+    
+
+
+# def verify_payment():
+#     completed()
+
 
