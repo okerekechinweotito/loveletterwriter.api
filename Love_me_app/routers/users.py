1ff51351b17f0294adb0fb683b22543fe930a292
YetundeAlabi <alabiyetunde007@gmail.com>
2022-11-19 12:16:58 +0100

    chore(BAC-7): tested the endpoints

--- Love_me_app/routers/users.py
+++ Love_me_app/routers/users.py
@@ -1,6 +1,6 @@
 from ..dependencies import get_current_user
 from fastapi import Depends, APIRouter, HTTPException
-from .. import schemas
+from .. import schemas, models
 from ..database import get_db
 from sqlalchemy.orm import Session 
 
@@ -10,8 +10,8 @@ router=APIRouter(tags=['User'],prefix="/api/v1/user/me")
 endpoint to get a user profile. The user has to be logged in already.
 
 """
-@router.get("/", dependencies=[Depends(get_current_user())])
-def user_me(user: schemas.User):
+@router.get("/")
+def user_me(user:dict=Depends(get_current_user)):
     if user is None:
         raise HTTPException(status_code=401, detail="user not found")
     return {
@@ -23,14 +23,16 @@ def user_me(user: schemas.User):
         "date_joined": user.date_created,
     }
 
+
 """
 endpoint to update user profile by getting the current user email and updating their profile.
 """
-@router.patch("/", dependencies=[Depends(get_current_user())])
-def update_profile(profile, user: schemas.User, db:Session = Depends(get_db)):
-    profile = user.email
-    profile.update(user.dict(exclude_unset=True))
+@router.patch("/",)
+def update_profile(request: schemas.UserBase, user:dict=Depends(get_current_user), db:Session = Depends(get_db)):
+    if not user:
+        raise HTTPException(status_code=401, detail="user not found")
+    profile = db.query(models.User).filter(models.User.id)
+    profile.update(request.dict(exclude_unset=True))
     db.commit()
     return {"User successfully updated"}
         
-
