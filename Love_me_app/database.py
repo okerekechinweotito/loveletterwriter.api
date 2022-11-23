fcaa97233214f83145d467c9b2a9142645daa686
madvirus-ops <edwinayabie1@gmail.com>
2022-11-23 18:16:02 +0100

    merge conflict

--- Love_me_app/database.py
+++ Love_me_app/database.py
@@ -1,12 +1,15 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./project.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+load_dotenv()
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./project.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},echo = True 
+    SQLALCHEMY_DATABASE_URL, echo = True 
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
