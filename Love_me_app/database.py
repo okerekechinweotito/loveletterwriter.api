ce80cb428f4385e5d85ec51c789c362750f529f3
idimmusix <idimmusix@gmail.com>
2022-11-23 15:50:13 +0000

    feat: added a postgresql database

--- Love_me_app/database.py
+++ Love_me_app/database.py
@@ -1,12 +1,14 @@
+import os
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./project.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./project.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},echo = True 
+    SQLALCHEMY_DATABASE_URL, echo = True 
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
