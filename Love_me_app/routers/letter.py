0b5fcf0a5bf917e85a5694c16634de0b6c9c503f
YetundeAlabi <alabiyetunde007@gmail.com>
2022-11-23 21:30:52 +0100

    feat(BAC-32): added schedule and sent letter endpoint

--- Love_me_app/routers/letter.py
+++ Love_me_app/routers/letter.py
@@ -1,18 +1,58 @@
-from fastapi import APIRouter,HTTPException,status
-from Love_me_app.business.letter import LetterBusiness
-from sqlalchemy.orm import Session
-from Love_me_app.database import get_db
-from fastapi import Depends
+"""sent_letters = []
+scheduled_letters = []
+letters = db.query(models.Letter).join(models.Schedule).all()
+for letter in letters:
+    if Schedule.schedule_time > datetime.now():
+        schedule_letters.append(letter)
+    else:
+        sent_letters.append(letter)
+
+
+"""
 from ..dependencies import get_current_user
+from fastapi import Depends, APIRouter, HTTPException
+from .. import schemas, models
+from ..database import get_db
+from sqlalchemy.orm import Session 
+
+# initialize router
+router = APIRouter(prefix="/api/v1/letter")
+db = get_db()
+
+# function to get all letters
+@router.get("/")
+async def get_all_letter(user:dict = Depends(get_current_user), db:Session=Depends(get_db)):
+    sent_letters = []
+    scheduled_letters = []
+    # raise a user not found error if user details is not correct
+    if not user :
+        raise HTTPException(status_code=401, detail="User not Found") 
+    # get all user letters from the database
+    letters = db.query(models.Letter).filter(models.Letter.user_id).all()
+    return letters
+    
+@router.get("/sent")
+def get_sent_letter(user:dict=Depends(get_current_user), db:Session=Depends(get_db)):
+    # sent_letters = []
+    if user:
+        sent_letters = db.query(models.Letter).filter(is_sent=True).all()
+        # if models.Letter.is_sent == True:
+
+        # for letter in letters:
+        #     sent_letters.append(letter)
+        return sent_letters
+    else:
+        raise HTTPException(status_code=401, detail="User not Found")
 
 
-router = APIRouter(tags=['letter'],prefix="/api/v1/letter")
+@router.get("/scheduled")
+def get_scheduled_letter(user:dict=Depends(get_current_user), db:Session=Depends(get_db)):
+    # sent_letters = []
+    if user:
+        # get all scheduled_letters in the db 
+        scheduled_letters = db.query(models.Letter).filter(models.Letter.user_id).filter(is_scheduled=True).all()
+        return scheduled_letters
+    else:
+        raise HTTPException(status_code=401, detail="User not Found")
 
 
-@router.post("/{receiver_id}")
-async def generate_letter(receiver_id,user:dict=Depends(get_current_user), db:Session = Depends(get_db),):
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Please log in")
-    user_id = user.id
-    api_response = LetterBusiness.generate_letter(user_id, receiver_id,db)
-    return api_response
