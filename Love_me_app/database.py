1b6f3d1c8c380be911ce166eaaf6f62293393f69
seyiadel <seyiadel03@gmail.com>
2022-12-01 11:19:05 +0100

    removed connect_args in engine for postgres

--- Love_me_app/database.py
+++ Love_me_app/database.py
@@ -10,7 +10,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False} , echo = True 
+    SQLALCHEMY_DATABASE_URL, echo = True 
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
