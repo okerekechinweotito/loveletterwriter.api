8f48abfe2154a38ab1b540c910d83958cab1537e
Eva-Wamuyu <rutheve.eva@gmail.com>
2022-12-12 11:10:43 +0300

    modify models

--- Love_me_app/models.py
+++ Love_me_app/models.py
@@ -24,14 +24,14 @@ class User(Base):
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     free_trial = Column(Boolean,default=True)
     last_active = Column(DateTime(timezone=True), server_default=func.now(),nullable=True)
-    receiver = relationship('Receiver', back_populates='sender')
+    receiver = relationship('Receiver', back_populates='sender',cascade="all, delete-orphan")
     letter = relationship('Letter', back_populates='writer')
-    schedule = relationship('Schedule', back_populates='user')
-    ai_trainer_value = relationship('AiTrainerValue', back_populates='user')
-    transaction = relationship('Transaction', back_populates='user')
-    reset_pass = relationship('ResetPass', back_populates='user')
-    product_review = relationship('ProductReview', back_populates='user')
-    feedback = relationship('Feedback', back_populates='user')
+    schedule = relationship('Schedule', back_populates='user',cascade="all, delete-orphan")
+    ai_trainer_value = relationship('AiTrainerValue', back_populates='user',cascade="all, delete-orphan")
+    transaction = relationship('Transaction', back_populates='user',cascade="all, delete-orphan")
+    reset_pass = relationship('ResetPass', back_populates='user',cascade="all, delete-orphan")
+    product_review = relationship('ProductReview', back_populates='user',cascade="all, delete-orphan")
+    feedback = relationship('Feedback', back_populates='user',cascade="all, delete-orphan")
 
 
 class Receiver(Base):
@@ -40,7 +40,7 @@ class Receiver(Base):
     name = Column(String(255))
     email = Column(String(255))
     phone_number = Column(String(255))
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     sender = relationship('User', back_populates='receiver')
     letter = relationship('Letter', back_populates='receiver')
@@ -50,7 +50,7 @@ class Receiver(Base):
 class Letter(Base):
     __tablename__ = "letters"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     receiver_id = Column(Integer, ForeignKey('receivers.id'), nullable=True)
     title = Column(TEXT)
     letter = Column(TEXT)
@@ -64,7 +64,7 @@ class Letter(Base):
 class Schedule(Base):
     __tablename__ = "schedules"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     receiver_id = Column(Integer, ForeignKey('receivers.id'))
     letter_id=Column(Integer,ForeignKey('letters.id'))
     schedule_time = Column(DateTime(timezone=True))
@@ -87,7 +87,7 @@ class AiTrainerValue(Base):
     __tablename__ = "ai_trainer_values"
     id = Column(Integer, primary_key=True, index=True)
     ai_trainer_id = Column(Integer, ForeignKey('ai_trainers.id'))
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     receiver_id = Column(Integer, ForeignKey('receivers.id'))
     value = Column(String(255))
     date_created = Column(DateTime(timezone=True), server_default=func.now())
@@ -116,7 +116,7 @@ class Subscription(Base):
 class Transaction(Base):
     __tablename__ = "transactions"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     subscription_id = Column(Integer, ForeignKey('subscriptions.id'))
     ref_no = Column(String(255))
     date_created = Column(String(255))
@@ -129,7 +129,7 @@ class Customer(Base):
     """to store stripe customer id"""
     __tablename__ = 'customer'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     customer_id = Column(String(255),unique=True)
 
 
@@ -137,8 +137,9 @@ class CustomerSubscription(Base):
     """to store stripe customer id"""
     __tablename__ = 'customer_subscription'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     subscription_id = Column(String(255),unique=True)
+    user = relationship('User', back_populates='customer_subscription')
 
 
     
@@ -147,7 +148,7 @@ class ResetPass(Base):
     __tablename__ = "reset_pass"
     id = Column(Integer, primary_key=True, index=True)
     pin = Column(String(255))
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     is_used = Column(Boolean)
     expiry_date = Column(DateTime)
     date_created = Column(DateTime(timezone=True), server_default=func.now())
@@ -163,7 +164,7 @@ class BlackListedTokens(Base):
 class ProductReview(Base):
     __tablename__ = "product_reviews"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     review = Column(String(255))
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     user = relationship('User', back_populates='product_review')
@@ -211,7 +212,7 @@ class Admin(Base):
 class Feedback(Base):
     __tablename__ = "feedback_response"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     is_helpfull = Column(Boolean)
     feedback = Column(String(255))
     user = relationship('User', back_populates='feedback')
@@ -221,7 +222,7 @@ class Feedback(Base):
 class ChatBot(Base):
     __tablename__ = "chat_bot"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id',ondelete='CASCADE'))
     human = Column(TEXT)
     ai = Column(TEXT)
     date_created = Column(DateTime(timezone=True), server_default=func.now())
